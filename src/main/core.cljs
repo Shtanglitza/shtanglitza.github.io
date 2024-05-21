3d7ce1882fd066f1a8f8c0d984ad82a2211799dc
Djordje <muta93srb@hotmail.com>
2024-05-21 12:00:02 +0200

    safari flexbox + img fix try 0

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -218,7 +218,7 @@
        "Who are we?"]
       [:img {:src (str assets-url "images/who-are-we.png")
              :style {:height 80
-                     :width "max-content"
+                     :width "fit-content"
                      :margin-left "auto"
                      :margin-right "auto"
                      :margin-top 20
@@ -248,7 +248,7 @@
        "Capabilities"]
       [:img {:src (str assets-url "images/capabilities.png")
              :style {:height 80
-                     :width "max-content"
+                     :width "fit-content"
                      :margin-left "auto"
                      :margin-right "auto"
                      :margin-top 20
@@ -294,7 +294,7 @@
         "1. Ontology Development and Management"]
        [:img {:src (str assets-url "images/ontology-development-and-management.png")
               :style {:height 80
-                      :width "max-content"
+                      :width "fit-content"
                       :margin-left "auto"
                       :margin-right "auto"
                       :margin-top 20
@@ -324,7 +324,7 @@
         "2. Data Integration and Harmonization"]
        [:img {:src (str assets-url "images/data-integration-and-harmonization.png")
               :style {:height 80
-                      :width "max-content"
+                      :width "fit-content"
                       :margin-left "auto"
                       :margin-right "auto"
                       :margin-top 20
@@ -353,7 +353,7 @@
         "3. Knowledge Graph Construction and Visualization"]
        [:img {:src (str assets-url "images/knowledge-graph-construction-and-visualization.png")
               :style {:height 80
-                      :width "max-content"
+                      :width "fit-content"
                       :margin-left "auto"
                       :margin-right "auto"
                       :margin-top 20
@@ -383,7 +383,7 @@
         "4. Data Analysis and Insights"]
        [:img {:src (str assets-url "images/data-analysis-and-insights.png")
               :style {:height 80
-                      :width "max-content"
+                      :width "fit-content"
                       :margin-left "auto"
                       :margin-right "auto"
                       :margin-top 20
@@ -412,7 +412,7 @@
         "5. Semantic Data Querying and Retrieval"]
        [:img {:src (str assets-url "images/semantic-data-querying-and-retrieval.png")
               :style {:height 80
-                      :width "max-content"
+                      :width "fit-content"
                       :margin-left "auto"
                       :margin-right "auto"
                       :margin-top 20
@@ -442,7 +442,7 @@
         "6. Data Security and Compliance"]
        [:img {:src (str assets-url "images/data-security-and-compliance.png")
               :style {:height 80
-                      :width "max-content"
+                      :width "fit-content"
                       :margin-left "auto"
                       :margin-right "auto"
                       :margin-top 20
@@ -471,7 +471,7 @@
         "7. Custom Biomedical Application Development"]
        [:img {:src (str assets-url "images/custom-biomedical-application-development.png")
               :style {:height 80
-                      :width "max-content"
+                      :width "fit-content"
                       :margin-left "auto"
                       :margin-right "auto"
                       :margin-top 20
@@ -501,7 +501,7 @@
         "8. Personalized Training and Support"]
        [:img {:src (str assets-url "images/personalized-training-and-support.png")
               :style {:height 80
-                      :width "max-content"
+                      :width "fit-content"
                       :margin-left "auto"
                       :margin-right "auto"
                       :margin-top 20
